ec1638e40e1b1a34b2129f092e44fade254a0192
ashikali1010 <ashik.ict.iu@gmail.com>
2024-04-28 13:30:17 -0500

    added filter functionality

--- src/components/MyArtCraftList/MyArtCraftList.jsx
+++ src/components/MyArtCraftList/MyArtCraftList.jsx
@@ -1,4 +1,4 @@
-import { useContext, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { Link, useLoaderData } from "react-router-dom";
 import { AuthContext } from "../../AuthProvider/AuthProvider";
 import MyListCard from "../MyListCard/MyListCard";
@@ -9,20 +9,62 @@ const MyArtCraftList = () => {
     const { user } = useContext(AuthContext);
     const lodedItems = useLoaderData();
     const [items, setItems] = useState(lodedItems);
-    console.log(items.length);
 
-    const myItems = items.filter(item => item.uid === user?.uid);
-    console.log(myItems);
+    const [getData, setgetData] = useState([]);
+    const [lodedData, setLodedData] = useState([]);
+
+    useEffect(() => {
+        fetch('https://assignment-10-server-site-beta.vercel.app/items')
+            .then(res => res.json())
+            .then(data => {
+                const filterData = data.filter(item => item.uid === user?.uid);
+                setLodedData(filterData);
+                setgetData(filterData);
+
+            })
+    }, [])
+
+
+    const handleOnSubmit = e => {     
+        const value = e.target.value;
+        console.log(value);
+
+        if (value === 'Yes') {
+            setLodedData(getData);
+           const newData = lodedData.filter(item => item.customization === 'Yes');
+            setLodedData(newData);
+            return;
+        }
+        else if (value === 'No') {
+            setLodedData(getData);
+            const newData = lodedData.filter(item => item.customization === 'No');
+            setLodedData(newData);
+            return;
+        }
+    }
+
+
 
     return (
-        <div className="h-[80vh]">
+        <div className="min-h-[80vh]">
             <h1 className='bg-slate-300 my-8 text-center pt-4 lg:pt-7 h-[70px] lg:h-[100px] text-3xl font-bold'>
                 My Art & Craft List</h1>
 
-            { items.length > 1?
+            <h1></h1>
+
+            <div className="text-center mb-10">
+                <select onClick={handleOnSubmit} className="text-lg select select-info w-full max-w-[250px]">
+                    <option disabled selected>Select Customization</option>
+                    <option>Yes</option>
+                    <option>No</option>
+                </select>
+
+            </div>
+
+            {lodedData.length > 0 ?
                 <div className="mb-32">
                     {
-                        myItems.map(item => <MyListCard
+                        lodedData.map(item => <MyListCard
                             key={item._id}
                             item={item}
                             items={items}
